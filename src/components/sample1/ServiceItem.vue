bada87dd4838849b985e0ddcdab0c1ad64ca9419
jake <jake1101dev@gmail.com>
2022-05-13 15:57:30 +0900

    a

---
+++ src/components/sample1/ServiceItem.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+defineProps([]);
+const onClick = () => {
+  console.log(1);
+};
+</script>
+
+<template>
+  <div class="service-item" @click="onClick">
+    <div class="service-item-alpha"></div>
+    <div
+      class="bg-[url(@/assets/images/icon1.png)] bg-center w-full h-full"
+    ></div>
+  </div>
+</template>
